c5a203008a4e4549116b63f2f607c708506eeabf
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-05-02 14:10:55 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+function showRoom (image: Image, Exits: any[]) {
+	
+}
+function Init () {
+    Rooms = [images.createImage(`
+        # . . # #
+        . # . . #
+        # . . . .
+        . . . . .
+        . . . . .
+        `), images.createImage(`
+        # . . # #
+        . # . . #
+        # . . . .
+        . . . . .
+        . . . . .
+        `), images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)]
+}
+let Rooms: Image[] = []
+Init()
