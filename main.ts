223e4383bca1822f6b86573934055582db06e5e3
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-06-21 15:58:10 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 function showRoom (image: Image) {
+    svroom = image
     x = 2
     y = 2
     basic.pause(100)
@@ -13,6 +14,9 @@ function showRoom (image: Image) {
     image.showImage(0)
     if (randint(0, 10) < 4) {
         placeGold()
+    }
+    if (randint(0, 10) > 4) {
+        placeMonster()
     }
 }
 input.onGesture(Gesture.LogoUp, function () {
@@ -37,6 +41,9 @@ input.onGesture(Gesture.TiltLeft, function () {
         showRoom(Rooms[randint(0, Rooms.length - 1)])
     }
 })
+function placeMonster () {
+    led.plotBrightness(1, 3, 90)
+}
 input.onGesture(Gesture.TiltRight, function () {
     if (255 != led.pointBrightness(x + 1, y)) {
         led.unplot(x, y)
@@ -108,9 +115,26 @@ input.onGesture(Gesture.LogoDown, function () {
         showRoom(Rooms[randint(0, Rooms.length - 1)])
     }
 })
+let svroom: Image = null
 let Rooms: Image[] = []
 let y = 0
 let x = 0
+images.createBigImage(`
+    # # . . . . . . . .
+    # . # . . . . . . .
+    # # . . # # . . . .
+    . . . # . . . . . .
+    . . . . # # . . . .
+    `).scrollImage(1, 200)
+images.createBigImage(`
+    . . . . . . . . . .
+    . # . . . . . . . .
+    # # # . . . . . . .
+    . # . . . . . . . .
+    # . # . . . . . . .
+    `).scrollImage(1, 200)
+game.setScore(0)
+game.setLife(2)
 x = 2
 y = 2
 Init()
@@ -119,5 +143,15 @@ for (let value of Rooms) {
 }
 showRoom(Rooms[randint(0, Rooms.length - 1)])
 basic.forever(function () {
+    if (70 == led.pointBrightness(x, y)) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 54, 54, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+        game.addScore(randint(20, 50))
+        showRoom(svroom)
+    }
+    if (90 == led.pointBrightness(x, y)) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+        game.removeLife(1)
+        showRoom(svroom)
+    }
     led.plot(x, y)
 })
